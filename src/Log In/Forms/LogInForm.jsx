5dbe4f3420458f583f715e03fb0aba68776beae5
Balanoiu Nicolae Andrei <andreibalanoiu67@gmail.com>
2023-08-27 23:12:03 +0300

    Update

--- src/Log In/Forms/LogInForm.jsx
+++ src/Log In/Forms/LogInForm.jsx
@@ -2,52 +2,111 @@ import React from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
-import '../styles/styles.css'
-
+import "../styles/styles.css";
+import animation from "../../img/Texting (1).gif";
 
 const LoginForm = ({ handleSubmit, err }) => {
+  console.log(`
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀
+  ⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀
+  ⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀
+  ⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀
+  ⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀
+  ⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀
+  ⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀
+  ⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀
+  ⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀
+  ⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀
+  ⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⢠⣿⣿⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+`);
   return (
-    <div className="sidebar-show">
-      <div className="container container-fixed-xl container-modal">
-        {/* <a className="d-inline-block" href="/"> */}
-        <svg className="container-modal-close icon-color-gray-600 icon icon-size-32">
-          <use href="/images/icons/delete.svg#icon-delete"></use>
-        </svg>
-        {/* </a> */}
-
-        <div className="row">
-          <div className="col-12 col-md-5 mb-2 mb-md-0">
-            <div className="mb-2 mb-md-4">
-              {/* <a className="d-inline-block" href="/"> */}
-
-              <svg className="lb-logo">
-                <use href="/images/land-book-logo-2.svg#lb-logo"></use>
-              </svg>
-              {/* </a> */}
-            </div>
-            <h2 className="mb-1">Log in to Cocs Company</h2>
-            <p>
-              We sell coke all over the country, I don't have any for sale, I
-              really don't know what to write here, no kidding '.
-            </p>
-          </div>
-          <div className="col-12 col-md-6 offset-md-1">
-            <div className="border bg-white rounded-3 mb-3 p-4 shadow-sm">
-              <form onSubmit={handleSubmit}>
-                <Input type="email" placeholder="email" />
-                <Input type="password" placeholder="password" />
-                <Button>Sign in</Button>
-                {err && <span>Something went wrong</span>}
-              </form>
+    <div className="font-sans bg-grey-lighter flex flex-col w-full">
+      <div className="bg-white border-b">
+        <div className="container mx-auto px-4">
+          <div className="flex items-center justify-between py-4">
+            <div className="w-1/4 md:w-auto text-center text-2xl font-medium">
+              COX
             </div>
 
-            <p>
-              You don't have an account? <Link to="/signup">Register</Link>
-            </p>
+            <div className="w-1/4 md:w-auto md:flex text-right">
+              <div>Home-page</div>
+              <div>About us</div>
+              <div>Contact us</div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="website-backdrop"></div>
+      <div className="flex flex-col md:flex-row">
+        <div className="hidden md:block w-full md:w-1/2 bg-white">
+          <img className="opacity-75 pl-8 md:ml-32" src={animation} alt="" />
+        </div>
+        <div className="w-full md:w-1/2 bg-white">
+          <div className="flex flex-col items-center pt-12">
+            <h1 className="text-center">Log in</h1>
+            <h2 className="text-xl py-4">text</h2>
+            <form
+              onSubmit={handleSubmit}
+              className="w-full md:w-1/2 bg-white md:border shadow-lg px-8 pt-6 pb-8"
+            >
+              <div className="mb-4">
+                <label
+                  className="block text-grey-darker text-sm font-bold mb-2"
+                  htmlFor="username"
+                >
+                  Email
+                </label>
+                <Input
+                  className="rounded bg-grey-lighter border border-grey-lighter w-full py-2 px-3 mb-3 focus:outline-none focus:bg-white focus:border-grey"
+                  type="email"
+                  placeholder="Email address"
+                />
+              </div>
+
+              <div className="mb-6">
+                <label
+                  className="block text-grey-darker text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <Input
+                  className="rounded bg-grey-lighter border border-grey-lighter w-full py-2 px-3 mb-3 focus:outline-none focus:bg-white focus:border-grey"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+
+              <div className="flex items-center justify-between">
+                <Button
+                  className="bg-green hover:bg-green-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                >
+                  Log In
+                </Button>
+                <a className="inline-block ">
+                  Don't have an account ?
+                  <Link
+                    className="inline-block "
+                    style={{ marginLeft: "10px" }}
+                    to="/signup"
+                  >
+                    {" "}
+                    Sing up
+                  </Link>
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Sign Up/forms/RegistrationForm.jsx
+++ src/Sign Up/forms/RegistrationForm.jsx
@@ -1,57 +1,121 @@
 import React from "react";
+import "../styles/styles.css";
 import InputField from "../Components/InputField";
 import AvatarUpload from "../Components/AvatarUpload";
 import ActionButton from "../Components/ActionButton";
 import { Link } from "react-router-dom";
-import '../styles/styles.css'
+import animation from "../../img/Messaging.gif";
+//https://storyset.com/ animatie ca sa nu uit
 
 const RegistrationForm = ({ handleSubmit, loading, err }) => {
-    const handleEnterKeyPress = () => {
+  const handleEnterKeyPress = () => {
     handleSubmit();
   };
+
   return (
-    <div className="main">
-    <div className="container">
-      <div className="content">
-        <div className="container-descriere">
-        <h2 className="title">Sign Up to Cocs Company</h2>
-            <p>
-              We sell coke all over the country, I don't have any for sale, I
-              really don't know what to write here, no kidding '.
-            </p>
+    <div className="font-sans bg-grey-lighter flex flex-col w-full">
+      <div className="bg-white border-b">
+        <div className="container mx-auto px-4">
+          <div className="flex items-center justify-between py-4">
+            <div className="w-1/4 md:w-auto text-center text-2xl font-medium">
+              COX
+            </div>
+
+            <div className="w-1/4 md:w-auto md:flex text-right">
+              <div>Home-page</div>
+              <div>About us</div>
+              <div>Contact us</div>
+            </div>
+          </div>
         </div>
-        <div className="form">
-            <form onSubmit={handleSubmit} id="form">
-            <div className="border">
-            <h2 className="title">Sign Up</h2>
-  
-            <div className="form-group form-input">
-              <InputField type="text"    />
-              <label for="name" className="form-label">Your name</label>
-            </div>
-  
-            <div className="form-group form-input">
-              <InputField type="email"  />
-              <label for="name" className="form-label">Your email</label>
-            </div>
-  
-            <div className="form-group form-input">
-              <InputField type="password"  />
-              <label for="name" className="form-label">Your password</label>
-            </div>
-            <AvatarUpload />
-  
-            <div className="form-submit">
-              <ActionButton className="submit" disabled={loading} text="Sign up" onEnterKeyPress={handleEnterKeyPress} />               
-              <p className="vertify-booking">
-                You do have an account? <Link to="/login">Login</Link>
-              </p>
-            </div>
-            </div>
-          </form>
+      </div>
+      <div className="flex flex-col md:flex-row">
+        <div className="hidden md:block w-full md:w-1/2 bg-white">
+          <img className="opacity-75 pl-8 md:ml-32" src={animation} alt="" />
+        </div>
+        <div className="w-full md:w-1/2 bg-white">
+          <div className="flex flex-col items-center pt-12">
+            <h1 className="text-center">Sign U</h1>
+            <h2 className="text-xl py-4">text</h2>
+            <form
+              onSubmit={handleSubmit}
+              id="form"
+              className="w-full md:w-1/2 bg-white md:border shadow-lg px-8 pt-6 pb-8"
+            >
+              <div className="mb-4">
+                <label
+                  className="block text-grey-darker text-sm font-bold mb-2"
+                  htmlFor="username"
+                >
+                  Name
+                </label>
+                <InputField
+                  className="rounded bg-grey-lighter border border-grey-lighter w-full py-2 px-3 mb-3 focus:outline-none focus:bg-white focus:border-grey"
+                  type="text"
+                  placeholder="First name"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label
+                  className="block text-grey-darker text-sm font-bold mb-2"
+                  htmlFor="username"
+                >
+                  Email
+                </label>
+                <InputField
+                  className="rounded bg-grey-lighter border border-grey-lighter w-full py-2 px-3 mb-3 focus:outline-none focus:bg-white focus:border-grey"
+                  type="email"
+                  placeholder="Email address"
+                />
+              </div>
+
+              <div className="mb-6">
+                <label
+                  className="block text-grey-darker text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <InputField
+                  className="rounded bg-grey-lighter border border-grey-lighter w-full py-2 px-3 mb-3 focus:outline-none focus:bg-white focus:border-grey"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+
+              <div className="mb-6">
+                <AvatarUpload
+                  className="rounded bg-grey-lighter border border-grey-lighter w-full py-2 px-3 mb-3 focus:outline-none focus:bg-white focus:border-grey"
+                  type="password"
+                />
+              </div>
+
+              <div className="flex items-center justify-between">
+              <ActionButton
+  className="bg-green hover:bg-green-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+  type="button"
+  disabled={loading}
+  text="Sign up"
+  onEnterKeyPress={handleEnterKeyPress}
+/>
+                <a className="inline-block ">
+                  Already have an acount ?
+                  <Link
+                    className="inline-block "
+                    style={{ marginLeft: "10px" }}
+                    to="/login"
+                  >
+                    Signin
+                  </Link>
+                </a>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-)}
+  );
+};
+
 export default RegistrationForm;
