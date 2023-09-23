239e0dea64dab0207e7a6cb9e0498d4104c72b4b
Balanoiu Nicolae Andrei <andreibalanoiu67@gmail.com>
2023-09-23 21:42:40 +0300

    error fixing

--- src/Chat/components/Message.jsx
+++ src/Chat/components/Message.jsx
@@ -29,7 +29,7 @@ const Message = ({ message }) => {
         <span>just now</span>
       </div>
       <div className="messageContent">
-        <p>{message.text}</p>
+        <p style={{fontSize:'20px'}}>{message.text}</p>
         {message.img && <img src={message.img} alt="" />}
       </div>
     </div>
