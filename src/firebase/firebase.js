45c5b55dba100c006ed64e93ac9b462b6798e03c
Balanoiu Nicolae Andrei <andreibalanoiu67@gmail.com>
2023-10-15 15:09:06 +0300

    Update

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDk6f-iX5PZ95CMs1tQxZT-S0Gx3hp06ts",
@@ -9,10 +10,14 @@ const firebaseConfig = {
   projectId: "licenta-chatapp",
   storageBucket: "licenta-chatapp.appspot.com",
   messagingSenderId: "377378890485",
-  appId: "1:377378890485:web:59c986fbd99664ddad1bbc"
+  appId: "1:377378890485:web:59c986fbd99664ddad1bbc",
+  measurementId: "G-3VW17B4BCK"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore();
+export const analytics = getAnalytics(app);
+
+
