ea793063c394d21f834e1fc8ec70ebda9c2e089f
Balanoiu Nicolae Andrei <andreibalanoiu67@gmail.com>
2024-01-16 02:55:45 +0200

    add tailwind css

--- src/AES Encryption/decrypt.js
+++ src/AES Encryption/decrypt.js
@@ -3,18 +3,18 @@ import { decryptionKEY } from './decryptionKEY';
 
 export function decrypt(ciphertext) {
   if (ciphertext && ciphertext.startsWith('encrypted:')) {
-     console.log("Mesaj criptat:", ciphertext); 
+    //  console.log("Mesaj criptat:", ciphertext); 
      try {
        const decryptedData = AES.decrypt(ciphertext.slice(10), decryptionKEY);
        const decryptedMessage = decryptedData.toString(enc.Utf8);
-       console.log("Mesaj decriptat:", decryptedMessage);
+      //  console.log("Mesaj decriptat:", decryptedMessage);
        return decryptedMessage;
      } catch (error) {
        console.error("Decryption failed:", error);
        return null; 
      }
   } else {
-    console.log("Mesaj necriptat:", ciphertext);
+    // console.log("Mesaj necriptat:", ciphertext);
     return ciphertext;
   }
 }
