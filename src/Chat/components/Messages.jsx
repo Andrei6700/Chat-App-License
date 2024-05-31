12fdeecda6043c2a8312292e31d538be21fd8ca0
Balanoiu Nicolae Andrei <andreibalanoiu67@gmail.com>
2024-06-01 01:14:02 +0300

    improved readability and performance

--- src/Chat/components/Messages.jsx
+++ src/Chat/components/Messages.jsx
@@ -1,4 +1,10 @@
-import React, { useContext, useEffect, useState, useRef, useCallback } from "react";
+import React, {
+  useContext,
+  useEffect,
+  useState,
+  useRef,
+  useCallback,
+} from "react";
 import Message from "./Message";
 import { ChatContext } from "../../context/ChatContext";
 import { AuthContext } from "../../context/AuthContext";
@@ -20,30 +26,29 @@ const Messages = () => {
   const { theme } = useTheme();
   // Ref to the end of the messages list, used to scroll into view
   const messagesEndRef = useRef(null);
-  // State to store the limit of messages to fetch
-  const [limit, setLimit] = useState(20);
-// fetch messages from the Firestore
+
+  // fetch messages from the Firestore
   const fetchMessages = useCallback(() => {
     const unSub = onSnapshot(doc(db, "chats", data.chatId), async (docData) => {
       if (docData.exists()) {
+        // Mark messages as read if they are not sent by the current user and are unread
         const allMessages = docData.data().messages;
-                // Mark messages as read if they are not sent by the current user and are unread
         const newMessages = allMessages.map((message) => {
           if (message.senderId !== currentUser.uid && !message.read) {
             return { ...message, read: true };
           }
           return message;
         });
-                // Update the document in Firestore with the new messages
+        // Update the document in Firestore with the new messages
         await updateDoc(doc(db, "chats", data.chatId), {
           messages: newMessages,
         });
-                // Update the messages state with the new messages, limited by the limit state
-        setMessages(newMessages.slice(-limit)); 
+        // Update the messages state with the new messages, limited by the limit state
+        setMessages(newMessages); // No limit, load all messages
       }
     });
     return unSub;
-  }, [data.chatId, currentUser.uid, limit]);
+  }, [data.chatId, currentUser.uid]);
 
   // Fetch messages on component mount and clean up on unmount
   useEffect(() => {
@@ -53,28 +58,22 @@ const Messages = () => {
     };
   }, [fetchMessages]);
 
-    // Scroll to the end of the messages list whenever the messages state changes
+  // Scroll to the end of the messages list whenever the messages state changes
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
-
-  // Handle scroll event on the messages list
-  const handleScroll = (e) => {
-    // If the user has scrolled to the top, increase the limit to fetch more messages
-    if (e.target.scrollTop === 0) {
-      setLimit((prevLimit) => prevLimit + 20);
-    }
-  };
 
   const shouldShowDate = (index) => {
     if (index === 0) return true;
     const currentDate = messages[index].date.toDate();
     const previousDate = messages[index - 1].date.toDate();
-    return format(currentDate, "dd/MM/yyyy") !== format(previousDate, "dd/MM/yyyy");
+    return (
+      format(currentDate, "dd/MM/yyyy") !== format(previousDate, "dd/MM/yyyy")
+    );
   };
 
   return (
-    <div className={`messages ${theme}`} onScroll={handleScroll}>
+    <div className={`messages ${theme}`}>
       {messages.map((message, index) => {
         const decryptedMessage = decrypt(message.text);
         return (
